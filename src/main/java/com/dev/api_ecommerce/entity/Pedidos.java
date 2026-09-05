79926536aaa7a0d935e8c4ec70412cc4f4b23ae5
guiCherpinski <cherpinskiguilherme@gmail.com>
2026-09-05 15:10:27 -0300

    feat: Cria entity Pedidos

---
+++ src/main/java/com/dev/api_ecommerce/entity/Pedidos.java
@@ -0,0 +1,36 @@
+package com.dev.api_ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pedidos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Pedidos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            name = "data_pedido",
+            nullable = false
+    )
+    private LocalDate dataPedido;
+
+    @Column(
+            name = "status",
+            nullable = false,
+            length = 30
+    )
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id")
+    private Clientes cliente;
+}
