05ac3ca6c357b079d5557b61c3ca07360385718c
guiCherpinski <cherpinskiguilherme@gmail.com>
2026-09-05 13:24:20 -0300

    Primeiro commit

---
+++ src/test/java/com/dev/api_ecommerce/ApiEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.api_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
