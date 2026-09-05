a3b4aa0871e21d73e03bfb41db671d1bbef0a4c1
guiCherpinski <cherpinskiguilherme@gmail.com>
2026-09-05 14:41:39 -0300

    feat: Cria entity Clientes

---
+++ src/main/java/com/dev/api_ecommerce/entity/Clientes.java
@@ -0,0 +1,44 @@
+package com.dev.api_ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "clientes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Clientes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            name = "nome",
+            nullable = false,
+            length = 100
+    )
+    private String nome;
+
+    @Column(
+            name = "email",
+            nullable = false,
+            length = 100,
+            unique = true
+    )
+    private String email;
+
+    @Column(
+            name = "cpf",
+            nullable = false,
+            length = 14,
+            unique = true
+    )
+    private String cpf;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "enderecos")
+    private Enderecos enderecos;
+}
